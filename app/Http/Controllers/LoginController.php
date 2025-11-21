37f726b17f93ad7f0018150ebeebf144d44fda00
imran <077imrn@gmail.com>
2025-11-21 22:48:43 +0530

    ELA-7 made Ui and migrations for user

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('nic', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'failed' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [LoginController::class, 'index'])
+->name('login.index');
+Route::post('/login', [LoginController::class, 'authenticate'])
+->name('login.authenticate');
