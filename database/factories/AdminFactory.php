d8246647099d4a2193d43fe626511f7ebb809953
imran <077imrn@gmail.com>
2026-01-20 11:41:42 +0530

    --

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
@@ -14,6 +15,13 @@ class AdminFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password = null;
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Admin::class;
 
     /**
      * Define the model's default state.
